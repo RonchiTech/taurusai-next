3b31d82e59cf3477733ef1cc8e966ee11d01332f
ronchitech <ronchibusiness@gmail.com>
2024-08-25 14:33:50 +0800

    feat: add initial commit

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from 'next';
+import React from 'react';
+
+
+export const metadata: Metadata = {
+  title: 'Taurus AI',
+
+};
+
+function Dashboard() {
+  return <div>Dashboard</div>;
+}
+
+export default Dashboard;
